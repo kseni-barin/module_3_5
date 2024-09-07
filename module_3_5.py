d6fb3eee3932030c5ac4cd0f9d0591eb1d95e684
kseni-barin <kmalyutkina@yandex.ru>
2024-09-07 23:37:21 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,15 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        if int(str_number[1:]) != 0:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first
+    else:
+        return first
+
+result = get_multiplied_digits(402030070)
+print(result)
+
+
